58b6eb1d242230d708081c277e580ec121171a7a
selimyan <gselimyan@gmail.com>
2018-12-16 16:28:21 -0500

    initial css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
 
-class App extends Component {
+export default class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -10,5 +11,3 @@ class App extends Component {
     )
   }
 }
-
-export default App;
